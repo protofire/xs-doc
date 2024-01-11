3fe8f96a92992c968f2789901d28d8814183d557
Agustín Longoni <agustin.longoni@protofire.io>
2024-01-11 12:03:36 -0300

    Update home (#1)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,8 +3,8 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "XSigners",
+  tagline: "The most trusted decentralized multisig platform on Polkadot ecosystem",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
